e607d51e21461d298f456a2b73752e65331bbe56
roque <roque_gerez@hotmail.com>
2024-10-30 18:31:40 -0300

    agregando los cambios en mi casa

--- brunette/settings.py
+++ brunette/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'applications.caja',
+    # app locales
     'applications.users',
     'applications.cocina',
     'applications.home',
