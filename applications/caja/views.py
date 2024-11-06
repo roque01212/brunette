ae1aa2a64ac18484d6e37422a587dd259fab56ec
roque <roque_gerez@hotmail.com>
2024-11-05 23:18:03 -0300

    guardado comun

--- applications/caja/views.py
+++ applications/caja/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.forms import formset_factory
 from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 from.forms import CajaForm, CajaUpdateForm
@@ -25,10 +26,11 @@ class AperturaCaja(FormView):
     
 
     
-class CierreCaja(FormView):
+class CierreCaja(LoginRequiredMixin, FormView):
     form_class = CajaUpdateForm
     success_url = reverse_lazy('home_app:Index')
     template_name= 'caja/cierre_caja.html'
+    login_url = reverse_lazy('users_app:User-Login')
 
 
     def form_valid(self, form):
@@ -62,40 +64,7 @@ class CrearPedidoView(FormView):
             producto_id =get_object_or_404(Productos, id=producto)
             total_pedido = self.request.POST.get('total_pedido')
             DetallePedido.objects.crear_detalleP(pedido, producto_id, total_pedido)
-
+            Mesas.objects.update_mesa(mesa)
 
             return super().form_valid(form)
 
-
-    # # Formset para manejar múltiples productos en un solo pedido
-    # detalle_pedido_formset = formset_factory(DetallePedidoForm, extra=1, can_delete=False)
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['formset'] = self.detalle_pedido_formset()
-    #     return context
-
-    # def form_valid(self, form):
-    #     formset = self.detalle_pedido_formset(self.request.POST)
-    #     print(formset)
-    #     if formset.is_valid():
-    #         # Crear el pedido principal
-    #         caja = get_object_or_404(Caja, abierta_caja=True)  # Caja abierta
-    #         mesa = form.cleaned_data['mesa']
-    #         tipo_pago = form.cleaned_data['tipo_pago_pedido']
-    #         pagado = form.cleaned_data['pagado_pedido']
-    #         pedido = Pedidos.objects.crear_pedidos(caja, mesa, tipo_pago, pagado)
-            
-    #         # Guardar cada detalle del pedido
-    #         for detalle_form in formset:
-    #             print(detalle_form.cleaned_data.get('producto'))
-    #             producto = detalle_form.cleaned_data.get('producto')
-    #             total_pedido = detalle_form.cleaned_data.get('total_pedido')
-    #             if producto and total_pedido:
-    #                 DetallePedido.objects.crear_detalleP(pedido,producto, total_pedido)
-    #         # Mesas.objects.update_mesa(mesa)
-
-
-    #         return super().form_valid(form)
-    #     else:
-    #         return self.form_invalid(form)
